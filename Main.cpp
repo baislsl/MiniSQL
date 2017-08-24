e60cbe2c17f4af64cc2d105b15beed24d4ee56cd
baislsl <baislsl666@gmail.com>
2017-08-24 23:21:25 +0800

    remove redundant test code in Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,10 +4,7 @@
 
 #include <iostream>
 #include "MiniSQL/Interpreter/Interpreter.h"
-#include "MiniSQL/index/BP_node.h"
 
 int main() {
-    // std::ifstream in("../Test/run.sql");
-    BP_node node;
     Interpreter interpreter(std::cin, std::cout);
 }
